7342d3a01e04c42ba0e253ededf33ab161dcdf7e
Lily <55459334+FlavoredSaucer@users.noreply.github.com>
2024-06-11 02:00:45 -0500

    team iff 0 now workies

--- what.py
+++ what.py
@@ -2,6 +2,12 @@ import random as rng
 from re import split
 from time import sleep
 
+
+def eep(tim : float = 0):
+	if tim >= 0.05:
+		sleep(tim)
+
+
 class Die:
 	def __init__(self, nos):
 		self.NumberOfSides = nos
@@ -20,7 +26,7 @@ class PlayerGeneric:
 	LastDamage = 0
 
 	Name = "PlayerGeneric"
-	IFF = 0
+	iFF = 0
 	d20 = Die(20)
 	InitiativePlus = 0
 	Team = None
@@ -31,6 +37,7 @@ class PlayerGeneric:
 	Opponent = None
 	DeathMessage = False
 	PlusToHit = 0
+	AttackActions = 1
 	Attack = "0d0+2"
 	AtkDice = []
 	AtkPlus = 0
@@ -42,13 +49,17 @@ class PlayerGeneric:
 	]
 
 
-	def __init__(self, NameSuffix = ""):
+	def __init__(self,NameOverride : str = "", NameSuffix : str = "", IFFOverride : int = None):
 		self.d20 = Die(20)
+		if NameOverride:
+			self.Name = NameOverride
 		self.Name += NameSuffix
 		self.AtkDice = []
 		self.IsAlive = True
 		self.LastDamage = 0
 		self.Opponent = None
+		if IFFOverride is not None:
+			self.iFF = IFFOverride
 		asd = split('d|\+',self.Attack)
 		for i in range(int(asd[0])*2):
 			self.AtkDice.append(Die(int(asd[1])))
@@ -85,42 +96,58 @@ class PlayerGeneric:
 			self.IsAlive = False
 
 	def CommentateGenericAttack(self):
-		if self.Opponent is None:
-			print(self.Name+" cannot find a target!");sleep(slprshort/3)
-			return
-		if self.Opponent.HP <= 0:
-			self.FindOpponent()
-			if self.Opponent is None: return
-		self.DoAttack(self.Opponent)
-		if self.d20.Roll+self.PlusToHit >= self.Opponent.AC:
-			if self.d20.Roll == 20:
-				print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" (Nat 20) and CRITS! ",end="")
+		Attacks = self.AttackActions
+		while Attacks >= 1:
+			Attacks -= 1
+			if self.Opponent is None:
+				print(self.Name+" cannot find a target!")
+				return
+			if self.Opponent.HP <= 0:
+				self.FindOpponent()
+				if self.Opponent is None: return
+			self.DoAttack(self.Opponent)
+			if self.d20.Roll+self.PlusToHit >= self.Opponent.AC:
+				if self.d20.Roll == 20:
+					print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" (Nat 20) and CRITS! ",end="")
+				else:
+					print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" and hits! ",end="")
+				print("Dealing "+str(self.LastDamage)+" points to "+self.Opponent.Name+". ("+str(self.Opponent.HP)+" HP Left.)"); eep(HitDelay)
 			else:
-				print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" and hits! ",end="")
-			print("Dealing "+str(self.LastDamage)+" points to "+self.Opponent.Name+". ("+str(self.Opponent.HP)+" HP Left.)"); sleep(slpshort)
-		else:
-			print(self.Name+" misses "+self.Opponent.Name+" with a "+str(self.d20.Roll+self.PlusToHit)+"..."); sleep(slpshort/2)
+				print(self.Name+" misses "+self.Opponent.Name+" with a "+str(self.d20.Roll+self.PlusToHit)+"..."); eep(MissDelay)
 
 
 	def Speak(self):
 		print(self.Name+" says: "+rng.choice(self.Phrases))
 	
 	def FindOpponent(self,Silent = True):
-		if self.IFF == 0:
-			EnemyTeams = [x for x in Teams if x.IsAlive and x != self]
+		EnemyTeams = []
+		if self.iFF == 0:
+			for x in Teams:
+				if x.IsAlive:
+					for t in x.Teammates:
+						if t.IsAlive and t is not self:
+							EnemyTeams.append(t)
 		else:
-			EnemyTeams = [x for x in Teams if x != self.Team and x.IsAlive]
+			for x in Teams:
+				if x.IsAlive and x.TeamIFF != self.iFF:
+					for t in x.Teammates:
+						if t.IsAlive:
+							EnemyTeams.append(t)
+		
 		if len(EnemyTeams) == 0:
 			self.Opponent = None
 			return
-		self.Opponent = rng.choice(list(EnemyTeams)).ReturnRandomTeammate()
-		if not Silent:
-			print(self.Name+"'s opponent is "+self.Opponent.Name+"!")
+		else:
+			self.Opponent = rng.choice(EnemyTeams)
+			if not Silent:
+				print(self.Name+"'s opponent is "+self.Opponent.Name+"!")
+			return
+
 
 
 class PlayerArwin(PlayerGeneric):
 	Name="Arwin"
-	IFF = 1
+	iFF = 1
 	AC=16
 	HP=34
 	InitiativePlus = 1
@@ -129,15 +156,24 @@ class PlayerArwin(PlayerGeneric):
 
 class PlayerPrincess(PlayerGeneric):
 	Name="Princess Ophelia"
-	IFF = 1
+	iFF = 1
 	AC=18
 	HP=40
 	PlusToHit=3
 	Attack="1d8+1"
 
+class PlayerPrincess2(PlayerGeneric):
+	Name="Princess Ophelia"
+	iFF = 1
+	AC=18
+	HP=76
+	AttackActions=2
+	PlusToHit=5
+	Attack="1d8+1"
+
 class PlayerAjay(PlayerGeneric):
 	Name="Ajay Gale"
-	IFF = 1
+	iFF = 1
 	AC=11
 	HP=20
 	InitiativePlus=4
@@ -146,7 +182,7 @@ class PlayerAjay(PlayerGeneric):
 
 class PlayerTaz(PlayerGeneric):
 	Name="Taaz"
-	IFF = 1
+	iFF = 1
 	AC=19
 	HP=28
 	InitiativePlus=1
@@ -155,7 +191,7 @@ class PlayerTaz(PlayerGeneric):
 
 class PlayerMartin(PlayerGeneric):
 	Name="Martin Magfiel"
-	IFF = 1
+	iFF = 1
 	AC=15
 	HP=35
 	InitiativePlus = 2
@@ -164,16 +200,17 @@ class PlayerMartin(PlayerGeneric):
 
 class PlayerArwin2(PlayerGeneric):
 	Name="Arwin Gepetto"
-	IFF = 1
+	iFF = 1
 	AC = 19
 	HP = 164
 	InitiativePlus = 2
+	AttackActions = 2
 	PlusToHit = 9
-	Attack="4d6+12"
+	Attack="2d6+6"
 
 class PlayerAjay2(PlayerGeneric):
 	Name="Ajay Gale"
-	IFF = 1
+	iFF = 1
 	AC=19
 	HP=50
 	InitiativePlus=5
@@ -182,7 +219,7 @@ class PlayerAjay2(PlayerGeneric):
 
 class PlayerTaz2(PlayerGeneric):
 	Name="Taaz"
-	IFF = 1
+	iFF = 1
 	AC=21
 	HP=95
 	InitiativePlus=1
@@ -191,33 +228,34 @@ class PlayerTaz2(PlayerGeneric):
 
 class PlayerMartin2(PlayerGeneric):
 	Name="Martin Magfiel"
-	IFF = 1
+	iFF = 1
 	AC=18
 	HP=97
 	InitiativePlus = 2
 	PlusToHit=10
 	Attack="2d6+7"
 
-class EnemyRat1(PlayerGeneric):
+class EnemyRatSwarm(PlayerGeneric):
 	Name="Rat Swarm"
-	IFF = 2
+	iFF = 2
 	AC=10
 	HP=24
 	PlusToHit=2
 	Attack="2d6"
 
-class EnemyRat2(PlayerGeneric):
+class EnemyRatGay(PlayerGeneric):
 	Name="1000 Gay Rats"
-	IFF = 2
+	iFF = 2
 	AC=5
 	HP=500
+	AttackActions=69
 	InitiativePlus=2
-	PlusToHit=0
-	Attack="3d10"
+	PlusToHit=3
+	Attack="1d4"
 
-class EnemyRat3(PlayerGeneric):
+class EnemyRatGiant(PlayerGeneric):
 	Name="Giant Rat"
-	IFF = 2
+	iFF = 2
 	AC=12
 	HP=7
 	InitiativePlus=0
@@ -226,7 +264,7 @@ class EnemyRat3(PlayerGeneric):
 
 class EnemyBeholder(PlayerGeneric):
 	Name="Beholder"
-	IFF = 3
+	iFF = 3
 	AC=18
 	HP=180
 	PlusToHit=5
@@ -234,21 +272,59 @@ class EnemyBeholder(PlayerGeneric):
 
 class RandomRat(PlayerGeneric):
 	Name="Wild Rat"
-	IFF = 0
-	AC = 12
-	HP = 31
-	PlusToHit = 2
-	Attack = "3d4"
+	iFF = 0
+	AC = 5
+	HP = 12
+	PlusToHit = 0
+	Attack = "1d4"
 
+class RandomRat2(PlayerGeneric):
+	Name="Wild Rat"
+	iFF = 0
+	AC = 5
+	HP = 2
+	AttackActions = 2
+	PlusToHit = 0
+	Attack = "1d4"
 
+class EnemyDragon(PlayerGeneric):
+	Name="dragom"
+	iFF=69
+	AC=19
+	HP=225
+	AttackActions=3
+	PlusToHit=12
+	Attack="2d10+7"
 
 class EnemyWTF(PlayerGeneric):
-	Name="Beholder"
-	IFF = 3
-	AC=10
-	HP=1
-	PlusToHit=5
-	Attack="4d6"
+	Name="Beholder V2"
+	iFF = 3
+	AC=19
+	HP=180
+	AttackActions = 23
+	Attack="1d4"
+
+	def CommentateGenericAttack(self):
+		Attacks = self.AttackActions
+		while Attacks >= 1:
+			Attacks -= 1
+			if self.Opponent is None:
+				print(self.Name+" cannot find a target!")
+				return
+			if self.Opponent.HP <= 0:
+				self.FindOpponent()
+				if self.Opponent is None: return
+			self.DoAttack(self.Opponent)
+			if self.d20.Roll+self.PlusToHit >= self.Opponent.AC:
+				if self.d20.Roll == 20:
+					print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" (Nat 20) and CRITS! ",end="")
+				else:
+					print(self.Name+" rolls a "+str(self.d20.Roll+self.PlusToHit)+" and hits! ",end="")
+				print("Dealing "+str(self.LastDamage)+" points to "+self.Opponent.Name+". ("+str(self.Opponent.HP)+" HP Left.)"); eep(HitDelay)
+			else:
+				print(self.Name+" misses "+self.Opponent.Name+" with a "+str(self.d20.Roll+self.PlusToHit)+"..."); eep(MissDelay)
+				self.FindOpponent()
+
 
 class TeamGeneric():
 	Name = "Generic Team"
@@ -281,26 +357,41 @@ class TeamGeneric():
 
 
 
-slplong = 1
-slpshort = slplong/2
-slprshort = slplong/10
+# slplong = .1
+# slpshort = slplong/2
+# slprshort = slplong/10
+
+TurnDelay = 1
+MissDelay = 0.1
+DeadDelay = 0.1
+HitDelay = 0.1
+
 
 # Main Game
 Turns = 0 
 
 Players = []
-
-for play in range(1):
-	Players.append(PlayerAjay2(str(play)))
-	Players.append(PlayerArwin2(str(play)))
-	Players.append(PlayerTaz2(str(play)))
-	Players.append(PlayerMartin2(str(play)))
-
-
-for rat in range(3):
-	Players.append(RandomRat(" " + str(rat)))
-
 Teams = []
+
+
+# Players.append(PlayerPrincess2())
+
+Players.append(EnemyDragon())
+
+Players.append(PlayerAjay2())
+Players.append(PlayerArwin2())
+Players.append(PlayerTaz2())
+Players.append(PlayerMartin2())
+
+# for kat in range(20):
+# 	Players.append(PlayerPrincess2(str(kat),IFFOverride=0))
+
+# for rat in range(400):
+# 	Players.append(RandomRat(" " + str(rat)))
+
+# Players.append(EnemyWTF(IFFOverride=2))
+
+
 
 rng.shuffle(Players)
 
@@ -310,9 +401,9 @@ for Plr in Players:
 Players = sorted(Players, key=lambda PlayerGeneric: PlayerGeneric.Initiative, reverse=True)
 
 for plr in Players:
-	if plr.IFF not in list(t.TeamIFF for t in Teams):
-		Teams.append(TeamGeneric(plr.Name+"'s team",plr.IFF))
-	next(x for x in Teams if x.TeamIFF == plr.IFF).AddTeammate(plr)
+	if plr.iFF not in list(t.TeamIFF for t in Teams):
+		Teams.append(TeamGeneric(plr.Name+"'s team",plr.iFF))
+	next(x for x in Teams if x.TeamIFF == plr.iFF).AddTeammate(plr)
 	
 
 for t in Teams:
@@ -321,18 +412,32 @@ for t in Teams:
 for plr in Players:
 	plr.FindOpponent()
 
-
 while True:
+	AlivePlayers = [x for x in Players if x.IsAlive]
+	HitDelay = TurnDelay * 3 / sum(x.AttackActions for x in AlivePlayers)
+	MissDelay = HitDelay
+	DeadDelay = HitDelay
 	for t in Teams:
 		t.Update()
 		for p in t.Teammates:
 			if not p.IsAlive:
 				p.DeathMessage = True
-	if list(t.IsAlive for t in Teams).count(True) == 1:
-		for t in Teams:
-			if t.IsAlive:
-				print(t.Name+" wins!")
+	AliveTeams = [t for t in Teams if t.IsAlive]
+	if len(AliveTeams) == 1:
+		TeamTemp = AliveTeams[0]
+		if TeamTemp.TeamIFF == 0:
+			ZeroTeammates = [x for x in TeamTemp.Teammates if x.IsAlive]
+			if len(ZeroTeammates) == 1:
+				print(ZeroTeammates[0].Name+" wins!")
+				print(ZeroTeammates[0].Name+" has "+str(ZeroTeammates[0].HP)+" HP left")
 				break
+		else:
+			print(TeamTemp.Name+" wins!")
+			for t in TeamTemp.Teammates:
+				print(t.Name+" has "+str(t.HP)+" HP left")
+			break
+			
+	
 	# 		if t.IsAlive and t.TeamIFF == 0:
 	# 			if len(list(p.IsAlive for p in t.Teammates)) == 1:
 	# 				print("wawa")
@@ -343,10 +448,10 @@ while True:
 
 	Turns += 1
 
-	print("Turn "+str(Turns)+":"); sleep (slpshort)
+	print("Turn "+str(Turns)+":"); eep (TurnDelay)
 	for t in Teams:
 		if t.IsAlive:
-			print(t.Name+" is still up ",end=""); sleep(slprshort)
+			print(t.Name+" is still up ",end="")
 			numA = 0
 			numT = 0
 			for p in t.Teammates:
@@ -354,7 +459,7 @@ while True:
 				if p.IsAlive:
 					numA += 1
 			print("("+str(numA)+"/"+str(numT)+")")
-	print();sleep(slplong)
+	print();eep(TurnDelay)
 
 	for plr in Players:
 		for t in Teams:
@@ -363,10 +468,10 @@ while True:
 			plr.CommentateGenericAttack()
 		else:
 			if not plr.DeathMessage:
-				print(plr.Name+" is dead..."); sleep(slprshort)
+				print(plr.Name+" is dead..."); eep(DeadDelay)
 				plr.DeathMessage = True
-	print();sleep(slpshort)
-	print();sleep(slplong)
+	print();eep(DeadDelay)
+	print();eep(TurnDelay)
 
 
 
@@ -375,16 +480,16 @@ while True:
 # while True:
 # 	Turns += 1
 # 	print("	Round "+str(Turns)+"!")
-# 	sleep(.25)
+# 	eep(.25)
 # 	Player1.CommentateGenericAttack(Enemy1)
 # 	if not Enemy1.IsAlive:
-# 		print(Player1.Name+" has defeated "+Enemy1.Name+"!"); sleep(5)
+# 		print(Player1.Name+" has defeated "+Enemy1.Name+"!"); eep(5)
 # 		break
 # 	Enemy1.CommentateGenericAttack(Player1)
 # 	print("")
-# 	print(Player1.Name+"'s HP is "+str(Player1.HP)); sleep(.15)
-# 	print(Enemy1.Name+"'s HP is "+str(Enemy1.HP)); sleep(.15)
-# 	sleep(1)
+# 	print(Player1.Name+"'s HP is "+str(Player1.HP)); eep(.15)
+# 	print(Enemy1.Name+"'s HP is "+str(Enemy1.HP)); eep(.15)
+# 	eep(1)
 # 	if not Player1.IsAlive:
-# 		print(Player1.Name+" has died!"); sleep(5)
+# 		print(Player1.Name+" has died!"); eep(5)
 # 		break
